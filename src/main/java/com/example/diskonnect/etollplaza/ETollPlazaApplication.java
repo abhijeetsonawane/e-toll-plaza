230368dd88bc2115212e3ee7ea9c89a196300d3d
Abhijeet Sonawane <abhijeetsonawane@192.168.1.5>
2022-03-08 16:35:17 +0530

    Rejected remote .gitignore and used the local's instead
    Added a springboot project

---
+++ src/main/java/com/example/diskonnect/etollplaza/ETollPlazaApplication.java
@@ -0,0 +1,13 @@
+package com.example.diskonnect.etollplaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETollPlazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETollPlazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diskonnect/etollplaza/ETollPlazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diskonnect.etollplaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETollPlazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
